7ff8d0a4471d4eab47c3451451e8a54f42f95583
Ayush Kamboj <aayushkamboj7@gmail.com>
2020-12-05 00:55:28 +0530

    Assignment level 2

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,51 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt(`Enter your name`);
+alert(`Your user name is ${user}`);
+userName = user;
+console.log(`UserName = ${userName} and user= ${user}`);
+let age = 22, isLearning = true;
+user = "John";
+console.log(`UserName = ${userName} and user= ${user}`);
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = Number(prompt(`Enter first number`));
+let numB = Number(prompt(`Enter second number`));
+let sum = numA + numB;
+alert(sum);
+
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j = j+2) console.log(j);
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for(let numb=9 ; numb>0;numb = numb-2){
+    console.log(numb);
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
+let sum = ``;
+for (let num = 5; num>=0;num--){
+    sum= sum+num;
+}
+console.log(sum);
+
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+
+let sum = ``;
+for (let num = 10; num>=0;num=num-2){
+    sum= sum+num;
+}
+console.log(sum);
 
 // Comparisoin
 
@@ -36,3 +66,4 @@ Example:
 "hello", -21; // false
 
 */
+
